d6cc963b3f4f58f92064e69d820cd805135d165c
martinp95 <martinpelaezdiaz@gmail.com>
2018-04-02 16:23:14 +0200

    registrar y identificarse

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -5,6 +5,23 @@ module.exports = {
 		this.mongo = mongo;
 		this.app = app;
 	},
+	obtenerUsuarios : function(criterio, funcionCallback) {
+		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+			if (err) {
+				funcionCallback(null);
+			} else {
+				var collection = db.collection('usuarios');
+				collection.find(criterio).toArray(function(err, usuarios) {
+					if (err) {
+						funcionCallback(null);
+					} else {
+						funcionCallback(usuarios);
+					}
+					db.close();
+				});
+			}
+		});
+	},
 	insertarUsuario : function(usuario, funcionCallback) {
 		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
 			if (err) {
--- routes/rusers.js
+++ routes/rusers.js
@@ -6,24 +6,72 @@ module.exports = function(app, swig, gestorBD) {
 	});
 
 	app.post('/usuario', function(req, res) {
-		//Comprobar que las contraseñas coinciden
+		// Comprobar que las contraseñas coinciden
 		if (req.body.password == req.body.repetedPassword) {
-			var seguro = app.get("crypto").createHmac('sha256',
-					app.get('clave')).update(req.body.password).digest('hex');
-			var usuario = {
-				email : req.body.email,
-				password : seguro
+
+			// comprobar si ya esta en el sistema
+			var criterio = {
+				email : req.body.email
 			}
-			gestorBD.insertarUsuario(usuario, function(id) {
-				if (id == null) {
-					res.send("Error al insertar ");
+
+			gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+				// El usuario no existe, lo creo.
+				if (usuarios == null || usuarios.length == 0) {
+
+					var seguro = app.get("crypto").createHmac('sha256',
+							app.get('clave')).update(req.body.password).digest(
+							'hex');
+
+					var usuario = {
+						email : req.body.email,
+						password : seguro
+					}
+					gestorBD.insertarUsuario(usuario, function(id) {
+						if (id == null) {
+							res.send("Error al insertar ");
+						} else {
+							// meterlo en sesion se va a llevar a la vista de
+							// todos los usuarios de la aplicacion.
+							res.send('Usuario Insertado ' + id);
+						}
+					});
 				} else {
-					res.send('Usuario Insertado ' + id);
+					res.send('Error el usuario ya existe');
 				}
 			});
-		}else{
+		} else {
 			res.send("Error, las constraseñas no coinciden.");
 		}
+	});
+
+	app.get("/identificarse", function(req, res) {
+		var respuesta = swig.renderFile('views/bidentificacion.html', {});
+		res.send(respuesta);
+	});
+
+	app.post("/identificarse", function(req, res) {
+		var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+				.update(req.body.password).digest('hex');
+		var criterio = {
+			email : req.body.email,
+			password : seguro
+		}
+		gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+			if (usuarios == null || usuarios.length == 0) {
+				req.session.usuario = null;
+				res.send("No identificado: ");
+			} else {
+				req.session.usuario = usuarios[0].email;
+				res.send("identificado");
+				// falta redirigir a la vista que lista los usuarios de la
+				// aplicacion.
+			}
+		});
+	});
+
+	app.get('/desconectarse', function(req, res) {
+		req.session.usuario = null;
+		res.send("Usuario desconectado");
 	});
 
 };
