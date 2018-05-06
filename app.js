a5a606b22fab84017e1870ba9fffd0bc039a233a
Gemma González Gil <ggg-95@hotmail.com>
2018-05-06 22:39:15 +0200

    URLs RESTful (arreglado)

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ routerUsuarioSession.use(function (req, res, next) {
         next();
     } else {
         console.log("va a : " + req.session.destino)
-        res.redirect("/identificarse");
+        res.redirect("/user/identificarse");
     }
 });
 
